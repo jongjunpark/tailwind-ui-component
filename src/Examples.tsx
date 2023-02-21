59ea12797a8adf170b4d593b92c2310c7db618f6
박종준 <poiufgin7373@naver.com>
2023-02-22 00:50:38 +0900

    fix: item

--- src/Examples.tsx
+++ src/Examples.tsx
@@ -28,38 +28,40 @@ import ICONS from './icons'
  */
 
 export default function Examples() {
+  const dropdownItems = [
+    [
+      {
+        label: 'Edit',
+        icon: <ICONS.Edit tw="w-5 h-5 mr-2" />,
+        url: '/test',
+      },
+      {
+        label: 'Duplicate',
+        icon: <ICONS.Duplicate tw="w-5 h-5 mr-2" />,
+      },
+    ],
+    [
+      {
+        label: 'Archive',
+        icon: <ICONS.Archive tw="w-5 h-5 mr-2" />,
+      },
+      {
+        label: 'Move',
+        icon: <ICONS.Move tw="w-5 h-5 mr-2" />,
+      },
+    ],
+    [
+      {
+        label: 'Delete',
+        icon: <ICONS.Delete tw="w-5 h-5 mr-2" />,
+      },
+    ],
+  ]
+
   return (
     <Container>
       <Dropdown
-        items={[
-          [
-            {
-              label: 'Edit',
-              icon: <ICONS.Edit tw="w-5 h-5 mr-2" />,
-              url: '/test',
-            },
-            {
-              label: 'Duplicate',
-              icon: <ICONS.Duplicate tw="w-5 h-5 mr-2" />,
-            },
-          ],
-          [
-            {
-              label: 'Archive',
-              icon: <ICONS.Archive tw="w-5 h-5 mr-2" />,
-            },
-            {
-              label: 'Move',
-              icon: <ICONS.Move tw="w-5 h-5 mr-2" />,
-            },
-          ],
-          [
-            {
-              label: 'Delete',
-              icon: <ICONS.Delete tw="w-5 h-5 mr-2" />,
-            },
-          ],
-        ]}
+        items={dropdownItems}
         placement="bottom left"
         menuItemStyle={active => {
           return [
