46b3b01196e33aa1080776539cedb042348990e6
박종준 <poiufgin7373@naver.com>
2023-05-19 15:06:32 +0900

    fix: 불필요한 주석 및 파일 제거

--- src/Examples.tsx
+++ src/Examples.tsx
@@ -8,16 +8,9 @@ import {
   Sidebar,
   Breadcrumb,
   Select,
-  Autocomplete,
-  Toggle,
-  Disclosure,
-  RadioGroup,
-  Tabs,
-  Icons,
   Pagination,
 } from './components'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import TransitionDemo from './components/TransitionDemo'
 import ICONS from './icons'
 import usePagination from './hooks/usePagination'
 import { DropdownItems } from './samples/DropdownItems'
@@ -255,132 +248,6 @@ export default function Examples() {
           nextPage={nextPage}
         />
       </div>
-
-      {/* <Select
-        items={[
-          { name: 'Wade Cooper' },
-          { name: 'Arlene Mccoy' },
-          { name: 'Devon Webb' },
-          { name: 'Tom Cook' },
-          { name: 'Tanya Fox' },
-          { name: 'Hellen Schmidt' },
-        ]}
-      />
-
-      <Autocomplete
-        items={[
-          { name: 'Wade Cooper' },
-          { name: 'Arlene Mccoy' },
-          { name: 'Devon Webb' },
-          { name: 'Tom Cook' },
-          { name: 'Tanya Fox' },
-          { name: 'Hellen Schmidt' },
-        ]}
-      />
-
-      <Toggle
-        label="Enable notifications"
-        description="For extra noise"
-        groupProps={{}}
-        labelProps={{}}
-        descriptionProps={{}}
-        switchProps={{}}
-      />
-
-      <Disclosure
-        items={[
-          {
-            heading: 'What is your refund policy?',
-            content:
-              'If you’re unhappy with your purchase for any reason, email us within 90 days and we’ll refund you in full, no questions asked.',
-          },
-          {
-            heading: 'Do you offer technical support?',
-            content: 'No.',
-          },
-        ]}
-      />
-
-      <RadioGroup
-        screenReaderLabel="Server size"
-        items={[
-          {
-            name: 'Startup',
-            ram: '12GB',
-            cpus: '6 CPUs',
-            disk: '160 GB SSD disk',
-          },
-          {
-            name: 'Business',
-            ram: '16GB',
-            cpus: '8 CPUs',
-            disk: '512 GB SSD disk',
-          },
-          {
-            name: 'Enterprise',
-            ram: '32GB',
-            cpus: '12 CPUs',
-            disk: '1024 GB SSD disk',
-          },
-        ]}
-      />
-
-      <TransitionDemo />
-
-      <Tabs
-        items={{
-          Recent: [
-            {
-              id: 1,
-              title: 'Does drinking coffee make you smarter?',
-              date: '5h ago',
-              commentCount: 5,
-              shareCount: 2,
-            },
-            {
-              id: 2,
-              title: "So you've bought coffee... now what?",
-              date: '2h ago',
-              commentCount: 3,
-              shareCount: 2,
-            },
-          ],
-          Popular: [
-            {
-              id: 1,
-              title: 'Is tech making coffee better or worse?',
-              date: 'Jan 7',
-              commentCount: 29,
-              shareCount: 16,
-            },
-            {
-              id: 2,
-              title: 'The most innovative things happening in coffee',
-              date: 'Mar 19',
-              commentCount: 24,
-              shareCount: 12,
-            },
-          ],
-          Trending: [
-            {
-              id: 1,
-              title:
-                'Ask Me Anything: 10 answers to your questions about coffee',
-              date: '2d ago',
-              commentCount: 9,
-              shareCount: 5,
-            },
-            {
-              id: 2,
-              title: "The worst advice we've ever heard about coffee",
-              date: '4d ago',
-              commentCount: 1,
-              shareCount: 2,
-            },
-          ],
-        }}
-        tabGroupProps={{}}
-      /> */}
     </Container>
   )
 }
@@ -430,12 +297,6 @@ const exampleData: [string, TwStyle, string][] = [
     tw`from-white to-white`,
     '컨텐츠를 여러 페이지로 분리하여 탐색 및 이동할 수 있도록 도와주는데에 사용',
   ],
-  // ['Listbox (Select)', tw`from-amber-300 to-orange-500`],
-  // ['Switch (Toggle)', tw`from-green-400 to-cyan-500`],
-  // ['Disclosure', tw`from-fuchsia-500 to-purple-600`],
-  // ['Radio Group', tw`from-cyan-400 to-sky-500`],
-  // ['Transition', tw`from-pink-500 to-rose-500`],
-  // ['Tabs', tw`from-sky-400 to-blue-600`],
 ]
 
 type ExampleProps = {
