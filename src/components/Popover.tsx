f503a927e5e07125ade76c357a31981168203a32
박종준 <poiufgin7373@naver.com>
2023-06-07 14:09:34 +0900

    fix: popvoer twin-macro 제거 및 일부 수정

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,53 +1,68 @@
-import tw, { TwStyle } from 'twin.macro'
-import React, { useEffect, useState } from 'react'
+import type { Dispatch, SetStateAction, ElementType, ReactNode } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
 import { Popover as HeadlessPopover } from '@headlessui/react'
 import type * as PopperJS from '@popperjs/core'
 import { usePopper } from 'react-popper'
+
 import Transition from './Transition'
 
+import { cls } from '../utils/common'
+
 interface TransitionType {
-  enter?: TwStyle
-  enterFrom?: TwStyle
-  enterTo?: TwStyle
-  leave?: TwStyle
-  leaveFrom?: TwStyle
-  leaveTo?: TwStyle
+  enter?: string
+  enterFrom?: string
+  enterTo?: string
+  leave?: string
+  leaveFrom?: string
+  leaveTo?: string
 }
 
 interface PopoverProps {
-  content: React.ReactNode
+  content: ReactNode
   placement?: PopperJS.Placement
   offset?: number[]
+  close?: boolean // close와 setClose는 한세트
+  setClose?: Dispatch<SetStateAction<boolean>> // close와 setClose는 한세트
+  isInputChildren?: boolean
   panelProps?: {
-    as?: React.ElementType
+    as?: ElementType
     focus?: boolean
     static?: boolean
     unmount?: undefined
   }
+  panelStyle?: string
   transitionProps?: TransitionType
-  children: React.ReactNode
+  children?: ReactNode
 }
 
 const transitionPropsDefault = {
-  enter: tw`transition ease-out duration-200`,
-  enterFrom: tw`opacity-0 translate-y-1`,
-  enterTo: tw`opacity-100 translate-y-0`,
-  leave: tw`transition ease-in duration-150`,
-  leaveFrom: tw`opacity-100 translate-y-0`,
-  leaveTo: tw`opacity-0 translate-y-1`,
+  enter: 'transition ease-out duration-200',
+  enterFrom: 'opacity-0 translate-y-4',
+  enterTo: 'opacity-100 translate-y-0',
+  leave: 'transition ease-in duration-150',
+  leaveFrom: 'opacity-100 translate-y-0',
+  leaveTo: 'opacity-0 translate-y-4',
 }
 
-export default function Popover({
+export const Popover = ({
   content,
   placement = 'bottom-start',
   offset,
+  close,
+  setClose,
+  isInputChildren = false,
   panelProps,
+  panelStyle,
   transitionProps = transitionPropsDefault,
   children,
-}: PopoverProps) {
+}: PopoverProps) => {
   const [referenceElement, setReferenceElement] =
     useState<HTMLButtonElement | null>()
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>()
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isFocusContent, setIsFocusContent] = useState<boolean>(false)
+  const closeRef = useRef<HTMLDivElement>(null)
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
     placement,
     modifiers: [
@@ -58,26 +73,59 @@ export default function Popover({
     ],
   })
 
+  useEffect(() => {
+    if (close && setClose) {
+      closeRef.current?.click()
+      setClose(false)
+    }
+  }, [close, setClose])
+
   if (!content) return null
 
   return (
-    <HeadlessPopover tw="relative">
-      <>
-        <HeadlessPopover.Button ref={setReferenceElement}>
+    <div className="relative">
+      {isInputChildren && (
+        <button
+          type="button"
+          className="absolute z-10 w-full cursor-default"
+          onFocus={() => setIsOpen(true)}
+          onBlur={() => setIsOpen(false)}
+        >
           {children}
-        </HeadlessPopover.Button>
-        <Transition {...transitionProps}>
-          <HeadlessPopover.Panel
-            ref={setPopperElement}
-            style={styles.popper}
-            css={[tw`absolute z-10`]}
-            {...attributes.popper}
-            {...panelProps}
-          >
-            {content}
-          </HeadlessPopover.Panel>
-        </Transition>
-      </>
-    </HeadlessPopover>
+        </button>
+      )}
+      <HeadlessPopover className="relative">
+        {({ open, close }) => (
+          <>
+            <HeadlessPopover.Button
+              ref={setReferenceElement}
+              className="block w-full focus:outline-none"
+            >
+              {children}
+            </HeadlessPopover.Button>
+            <Transition
+              {...transitionProps}
+              // TODO: content가 살짝 깜빡이는 현상이 있음 추후 수정 예정
+              show={isInputChildren && isFocusContent ? true : isOpen || open}
+            >
+              <HeadlessPopover.Panel
+                ref={setPopperElement}
+                style={styles.popper}
+                className={cls('absolute z-10 min-w-full', panelStyle ?? '')}
+                onFocus={() => setIsFocusContent(true)}
+                onBlur={() => setIsFocusContent(false)}
+                {...attributes.popper}
+                {...panelProps}
+              >
+                <>
+                  <div onClick={close} ref={closeRef} />
+                  {content}
+                </>
+              </HeadlessPopover.Panel>
+            </Transition>
+          </>
+        )}
+      </HeadlessPopover>
+    </div>
   )
 }
