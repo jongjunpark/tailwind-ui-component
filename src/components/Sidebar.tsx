25ab687d442ba1cd9ec85961ea9ae8d07c24e261
박종준 <poiufgin7373@naver.com>
2023-06-07 14:09:50 +0900

    fix: sidebar twin-macro 제거 및 일부 수정

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,23 @@
-import tw, { TwStyle } from 'twin.macro'
-import React, { Fragment, useEffect, useState } from 'react'
-import { Dialog } from '@headlessui/react'
+import type { ReactNode } from 'react'
+import { Fragment, useEffect } from 'react'
+
 import Transition from './Transition'
-import ICONS from '../icons'
+
+import { cls } from '../utils/common'
 
 interface TransitionType {
-  enter?: TwStyle
-  enterFrom?: TwStyle
-  enterTo?: TwStyle
-  leave?: TwStyle
-  leaveFrom?: TwStyle
-  leaveTo?: TwStyle
+  enter?: string
+  enterFrom?: string
+  enterTo?: string
+  leave?: string
+  leaveFrom?: string
+  leaveTo?: string
 }
 
 type PlacementType = 'top' | 'bottom' | 'left' | 'right'
 
 interface SidebarProps {
-  children: React.ReactNode
+  children: ReactNode
   transitionProps?: TransitionType
   placement?: PlacementType
   open: boolean
@@ -24,30 +25,21 @@ interface SidebarProps {
 }
 
 const transitionPropsDefault = {
-  enter: tw`ease-out duration-300`,
-  enterFrom: tw`opacity-0`,
-  enterTo: tw`opacity-100`,
-  leave: tw`ease-in duration-200`,
-  leaveFrom: tw`opacity-100`,
-  leaveTo: tw`opacity-0`,
+  enter: 'ease-out duration-300',
+  enterFrom: 'opacity-0',
+  enterTo: 'opacity-100',
+  leave: 'ease-in duration-200',
+  leaveFrom: 'opacity-100',
+  leaveTo: 'opacity-0',
 }
 
-const itemTansitionPropsDefault = {
-  enter: tw`transform transition ease-in-out duration-300 sm:duration-300`,
-  enterFrom: tw`translate-x-full`,
-  enterTo: tw`translate-x-0`,
-  leave: tw`transform transition ease-in-out duration-300 sm:duration-300`,
-  leaveFrom: tw`translate-x-0`,
-  leaveTo: tw`translate-x-full`,
-}
-
-export default function Sidebar({
+export const Sidebar = ({
   children,
   transitionProps,
   placement = 'right',
   open = false,
   onClose,
-}: SidebarProps) {
+}: SidebarProps) => {
   useEffect(() => {
     if (open) {
       document.body.style.overflow = 'hidden'
@@ -59,74 +51,73 @@ export default function Sidebar({
   }, [open])
 
   const itemTansitionPropsDefault = {
-    enter: tw`transform transition ease-in-out duration-300 sm:duration-300`,
-    enterFrom: tw`translate-x-full`,
-    enterTo: tw`translate-x-0`,
-    leave: tw`transform transition ease-in-out duration-300 sm:duration-300`,
-    leaveFrom: tw`translate-x-0`,
-    leaveTo: tw`translate-x-full`,
+    enter: 'transform transition ease-in-out duration-300 sm:duration-300',
+    enterFrom: 'translate-x-full',
+    enterTo: 'translate-x-0',
+    leave: 'transform transition ease-in-out duration-300 sm:duration-300',
+    leaveFrom: 'translate-x-0',
+    leaveTo: 'translate-x-full',
   }
 
   if (placement === 'left') {
-    itemTansitionPropsDefault.enterFrom = tw`-translate-x-full`
-    itemTansitionPropsDefault.enterTo = tw`translate-x-0`
-    itemTansitionPropsDefault.leaveFrom = tw`translate-x-0`
-    itemTansitionPropsDefault.leaveTo = tw`-translate-x-full`
+    itemTansitionPropsDefault.enterFrom = '-translate-x-full'
+    itemTansitionPropsDefault.enterTo = 'translate-x-0'
+    itemTansitionPropsDefault.leaveFrom = 'translate-x-0'
+    itemTansitionPropsDefault.leaveTo = '-translate-x-full'
   } else if (placement === 'top') {
-    itemTansitionPropsDefault.enterFrom = tw`-translate-y-full`
-    itemTansitionPropsDefault.enterTo = tw`translate-y-0`
-    itemTansitionPropsDefault.leaveFrom = tw`translate-y-0`
-    itemTansitionPropsDefault.leaveTo = tw`-translate-y-full`
+    itemTansitionPropsDefault.enterFrom = '-translate-y-full'
+    itemTansitionPropsDefault.enterTo = 'translate-y-0'
+    itemTansitionPropsDefault.leaveFrom = 'translate-y-0'
+    itemTansitionPropsDefault.leaveTo = '-translate-y-full'
   } else if (placement === 'bottom') {
-    itemTansitionPropsDefault.enterFrom = tw`translate-y-full`
-    itemTansitionPropsDefault.enterTo = tw`translate-y-0`
-    itemTansitionPropsDefault.leaveFrom = tw`translate-y-0`
-    itemTansitionPropsDefault.leaveTo = tw`translate-y-full`
+    itemTansitionPropsDefault.enterFrom = 'translate-y-full'
+    itemTansitionPropsDefault.enterTo = 'translate-y-0'
+    itemTansitionPropsDefault.leaveFrom = 'translate-y-0'
+    itemTansitionPropsDefault.leaveTo = 'translate-y-full'
   }
 
   return (
-    <Fragment>
-      <Transition appear unmount={false} show={open} as={Fragment}>
-        <div tw="relative z-10">
+    <Transition appear unmount={false} show={open} as={Fragment}>
+      <div className="relative z-10">
+        <Transition.Child
+          as={Fragment}
+          appear
+          unmount={false}
+          {...transitionPropsDefault}
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div
+          id="sidebar-wrapper"
+          className="fixed inset-0 overflow-hidden"
+          onClick={e => {
+            if ((e.target as HTMLElement).id === 'sidebar-wrapper') {
+              onClose()
+            }
+          }}
+        >
           <Transition.Child
             as={Fragment}
             appear
             unmount={false}
-            {...transitionPropsDefault}
+            {...itemTansitionPropsDefault}
+            {...transitionProps}
           >
-            <div tw="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div tw="fixed inset-0 overflow-hidden" onClick={onClose}>
-            <Transition.Child
-              as={Fragment}
-              appear
-              unmount={false}
-              {...itemTansitionPropsDefault}
-              {...transitionProps}
+            <div
+              className={cls(
+                'absolute',
+                placement === 'right' ? 'right-0 top-0 h-full' : '',
+                placement === 'left' ? 'left-0 top-0 h-full' : '',
+                placement === 'top' ? 'inset-x-0 top-0 w-full' : '',
+                placement === 'bottom' ? 'inset-x-0 bottom-0 w-full' : '',
+              )}
             >
-              <div
-                css={[
-                  tw`absolute`,
-                  placement === 'right'
-                    ? tw`top-0 right-0 h-full`
-                    : placement === 'left'
-                    ? tw`top-0 left-0 h-full`
-                    : placement === 'top'
-                    ? tw`top-0 left-0 right-0 w-full`
-                    : tw`bottom-0 left-0 right-0 w-full`,
-                ]}
-                onClick={e => {
-                  e.stopPropagation()
-                  e.preventDefault()
-                }}
-              >
-                {children}
-              </div>
-            </Transition.Child>
-          </div>
+              {children}
+            </div>
+          </Transition.Child>
         </div>
-      </Transition>
-    </Fragment>
+      </div>
+    </Transition>
   )
 }
